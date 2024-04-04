0a3c75927d002c1f772b193a75a6f14c1d8762e5
Vaishnavi Gupta <vaishgupta1312@gmail.com>
2024-04-04 14:20:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello")      //we are using nodejs, not browser so it will give error, "alert not defined"
+// alert(3+3)
+
+console.log(3
+    +3) //code should be readable
+
+let name = "Seema"
+let age = "18"
+let isLoggedIn = true
+let state;
+
+// number=> 2^53
+// bigInt
+// string => ""
+//  boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique 
+
+// object
+
+console.log(typeof null);  //object 
+console.log(typeof undefined);  //undefined
